874ba1fde259a0b10ced4fb7fc7b44629fa9e0b2
Harsh-vardhan09 <harshvardhanp00@gmail.com>
2026-01-19 18:58:32 +0530

    Error Utility

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing=require("./models/listing.js");
-const path=require("path");
-const methodOverride=require("method-override");
-const ejsMate=require("ejs-mate")
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
-app.set("view engine",'ejs');
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs',ejsMate);
-app.use(express.static(path.join(__dirname,"/public"))); 
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
@@ -44,54 +46,94 @@ app.get("/", (req, res) => {
 //     await sampleListing.save();
 //     console.log("sample was saved");
 //     res.send("working");
-    
+
 // })
 
+
 //index route
-app.get("/listings",async(req,res)=>{
-    const allListing= await Listing.find({});
-    res.render("listing/index.ejs",{allListing})
-})
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    const allListing = await Listing.find({});
+    res.render("listing/index.ejs", { allListing });
+  }),
+);
+
 
 //new route
-app.get("/listings/new",(req,res)=>{
-    res.render("listing/new.ejs");
-})
+app.get("/listings/new", (req, res) => {
+  res.render("listing/new.ejs");
+});
+
 
 //show route
-app.get("/listings/:id",async(req,res)=>{
-    let {id}=req.params;
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listing/show.ejs",{listing})
-})
+    res.render("listing/show.ejs", { listing });
+  }),
+);
 
 //create route
-app.post("/listings",async(req,res)=>{
-    const newListing=new Listing(req.body.listing);
+app.post(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    if(!req.body.listing){
+      throw new ExpressError(400,"Send Valid Data");//this needs to be before the save since if it tries saving it will not give same error.
+    }
+    const newListing = new Listing(req.body.listing);
     await newListing.save();
-    res.redirect("/listings")
-});
+    res.redirect("/listings");
+  }),
+);
 
 //Edit route
-app.get("/listings/:id/edit",async(req,res)=>{
-    let {id}=req.params;
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listing/edit.ejs",{listing});
-})
+    res.render("listing/edit.ejs", { listing });
+  }),
+);
+
 
 //Update route
-
-app.put("/listings/:id",async(req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+app.put(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    if(!req.body.listing){
+      throw new ExpressError(400,"Send Valid Data");//this needs to be before the save since if it tries saving it will not give same error.
+    }
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect("/listings");
+  }),
+);
 
-});
 
 //Delete route
-
-app.delete("/listings/:id",async(req,res)=>{
-    let {id}=req.params;
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     await Listing.findByIdAndDelete(id);
-    res.redirect("/listings")
+    res.redirect("/listings");
+  }),
+);
+
+
+// for all route that doesn't exist
+app.use((req, res, next) => {
+  throw new ExpressError(404, "404 Page not found");
+});
+
+
+//error handling
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "something went wrong" } = err;
+  // res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs",{err});
 });
